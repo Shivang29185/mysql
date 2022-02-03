8d892a52f63033a9101a0d1871ccb5a138d21e6b
Shivang29185 <shivangbgohel@gmail.com>
2022-02-03 18:43:56 +0530

    final commit

--- company.sql
+++ company.sql
@@ -7,7 +7,7 @@ use company;
 -- create tables of company
 create table job(id int(10), name varchar(45), min_salary  decimal(10,2), max_salary decimal(10,2), primary key(id));
 create table country(id int(10), name varchar(45), primary key(id));
-create table department(id int(10), name varchar(45),fk_country_id int(10), primary key(id), foreign key(fk_country_id) references country(id));
+create table department(id int(10), name varchar(45), fk_country_id int(10), primary key(id), foreign key(fk_country_id) references country(id));
 create table employee(id int(10), firstname varchar(45), lastname varchar(45), email varchar(45), salary decimal(10,2),
 primary key(id), fk_department_id int(10), fk_job_id int(10), foreign key(fk_department_id) references department(id),
 foreign key(fk_job_id) references job(id));
@@ -40,19 +40,19 @@ insert into country(id, name) values (5, 'UK');
 insert into department(id, name, fk_country_id) values (1, 'computer', 1);
 insert into department(id, name, fk_country_id) values (2, 'It', 2);
 insert into department(id, name, fk_country_id) values (3, 'General', 3);
-insert into department(id, name, fk_country_id) values (4, 'Finance',4);
-insert into department(id, name, fk_country_id) values (5,'Testing',5);
+insert into department(id, name, fk_country_id) values (4, 'Finance', 4);
+insert into department(id, name, fk_country_id) values (5, 'Testing', 5);
 
 insert into employee(id, firstname, lastname, email, salary, fk_department_id, fk_job_id) 
-values (1,'Abhay', 'Pansuriya', 'abhaypansuriya@gmail.com', 18000, 1,1);
+values (1,'Abhay', 'Pansuriya', 'abhaypansuriya@gmail.com', 18000, 1, 1);
 insert into employee(id, firstname, lastname, email, salary, fk_department_id, fk_job_id) 
-values (2,'Himanshu', 'Bhavsar', 'himanshubhavshar@gmail.com', 22000, 2,2);
+values (2,'Himanshu', 'Bhavsar', 'himanshubhavshar@gmail.com', 22000, 2, 2);
 insert into employee(id, firstname, lastname, email, salary, fk_department_id, fk_job_id) 
-values (3, 'Dhruv', 'Acharya', 'dhruvacharya@gmail.com', 25000, 3,3);
+values (3, 'Dhruv', 'Acharya', 'dhruvacharya@gmail.com', 25000, 3, 3);
 insert into employee(id, firstname, lastname, email, salary, fk_department_id, fk_job_id) 
-values (4, 'Ravi', 'Dudhat', 'ravidudhat@gmail.com', 28000, 4,4);
+values (4, 'Ravi', 'Dudhat', 'ravidudhat@gmail.com', 28000, 4, 4);
 insert into employee(id, firstname, lastname, email, salary, fk_department_id, fk_job_id) 
-values (5, 'Shivang', 'Gohel', 'shivangbgohel@gmail.com', 24000, 5,5);
+values (5, 'Shivang', 'Gohel', 'shivangbgohel@gmail.com', 24000, 5, 5);
 
 -- get name (firstname + lastname) with department name, country name, job name
 SELECT 
